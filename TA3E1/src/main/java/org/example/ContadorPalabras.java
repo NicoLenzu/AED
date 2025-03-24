86d5bf0ca9c0195b59a4d7d4a98a46518494e3da
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-03-24 08:55:47 -0300

    Eje 2

--- TA3E1/src/main/java/org/example/ContadorPalabras.java
+++ TA3E1/src/main/java/org/example/ContadorPalabras.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class ContadorPalabras {
     public ContadorPalabras() {}
+
     public int contadorPalabras(String texto) {
         boolean palabra = false;
         int contador = 0;
@@ -16,4 +17,29 @@ public class ContadorPalabras {
         }
         return contador;
     }
+
+    public int cantidadMayorX(String texto, int caracteres){
+        boolean palabra = false;
+        boolean contado = false;
+        int contadorPalabras = 0;
+        int contadorCaracteres = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (Character.isLetter(texto.charAt(i))) {
+                palabra = true;
+                contadorCaracteres += 1;
+            }
+            if (!contado && contadorCaracteres == caracteres) {
+                contadorPalabras += 1;
+                contado = true;
+            }
+            if(palabra && (texto.charAt(i) == ' ' || i == texto.length() - 1 )){
+                contadorCaracteres = 0;
+                palabra = false;
+                contado = false;
+            }
+        }
+        return contadorPalabras;
+    }
 }
+
--- TA3E1/src/main/java/org/example/Main.java
+++ TA3E1/src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         ContadorPalabras contador = new ContadorPalabras();
-        System.out.println(contador.contadorPalabras("   a"));
+        System.out.println(contador.contadorPalabras("Hola Mundo"));
+        System.out.println(contador.cantidadMayorX("Hola Mundo asd a sdgfkjsdj    a123", 2));
     }
 
 }
