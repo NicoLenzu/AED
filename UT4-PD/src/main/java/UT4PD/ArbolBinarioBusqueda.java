fb2882e8505800bbe057fbea30463df750004b00
NicoLenzu <nicolas.lenzuen@correo.ucu.edu.uy>
2025-04-22 19:34:43 -0300

    Clase ArbolBinarioBusqueda

---
+++ UT4-PD/src/main/java/UT4PD/ArbolBinarioBusqueda.java
@@ -0,0 +1,28 @@
+package UT4PD;
+
+public class ArbolBinarioBusqueda<T> {
+    private NodoArbol<T> raiz;
+    public ArbolBinarioBusqueda(NodoArbol<T> r) {
+        raiz = r;
+    }
+    public NodoArbol<T> getRaiz() {
+        return raiz;
+    }
+    public NodoArbol<T> insertar(NodoArbol<T> nodoAct, NodoArbol<T> nodoIns) {
+        if (nodoAct == null) {
+            return nodoIns; // Si el nodo actual es null, este pasa a ser el nuevo nodo.
+        }
+        if (nodoIns.getId().compareTo(nodoAct.getDato()) > 0) {
+            nodoAct.setDer(insertar(nodoAct.getDer(), nodoIns)); // Actualizamos la derecha
+        } else if (nodoIns.getId().compareTo(nodoAct.getDato()) < 0) {
+            nodoAct.setIzq(insertar(nodoAct.getIzq(), nodoIns)); // Actualizamos la izquierda
+        }
+        return nodoAct; // Retornamos el nodo actualizado.
+    }
+
+
+
+
+
+
+}
---
+++ UT4-PD/src/main/java/UT4PD/NodoArbol.java
@@ -0,0 +1,94 @@
+package UT4PD;
+
+public class NodoArbol<T> {
+    private T dato;
+
+    public void setIzq(NodoArbol<T> izq) {
+        this.izq = izq;
+    }
+
+    public void setDer(NodoArbol<T> der) {
+        this.der = der;
+    }
+
+    private NodoArbol<T> izq;
+    private NodoArbol<T> der;
+
+    private Comparable<T> id;
+
+    public NodoArbol(T dato) {
+        this.dato = dato;
+        this.id = (Comparable<T>) dato;
+    }
+    public T getDato() {
+        return dato;
+    }
+    public NodoArbol<T> getDer() {
+        return der;
+    }
+    public NodoArbol<T> getIzq() {
+        return izq;
+    }
+    public Comparable<T> getId() {
+        return id;
+    }
+    public boolean esHoja() {
+        return izq == null && der == null;
+    }
+    public int altura(NodoArbol<T> nodoAct)
+    {
+        int A = -1;
+        int B = -1;
+        if (nodoAct.getIzq() != null) {
+            A = nodoAct.getIzq().altura(nodoAct.getIzq());
+        }
+        if (nodoAct.getDer() != null) {
+            B = nodoAct.getDer().altura(nodoAct.getDer());
+        }
+        return Math.max(A, B) + 1;
+    }
+    public int hojas(NodoArbol<T> nodoAct)
+    {
+        int hojas = 0;
+        if (nodoAct.esHoja()) {
+            hojas++;
+        }
+        if (nodoAct.getIzq() != null) {
+            hojas += nodoAct.getIzq().hojas(nodoAct.getIzq());
+        }
+        if (nodoAct.getDer() != null) {
+            hojas += nodoAct.getDer().hojas(nodoAct.getDer());
+        }
+        return hojas;
+    }
+    public int internos(NodoArbol<T> nodoAct)
+    {
+        int internos = 1; // contando la raiz
+        if (!nodoAct.esHoja()) {
+            internos++;
+        }
+        if (nodoAct.getIzq() != null) {
+            internos += nodoAct.getIzq().hojas(nodoAct.getIzq());
+        }
+        if (nodoAct.getDer() != null) {
+            internos += nodoAct.getDer().hojas(nodoAct.getDer());
+        }
+        return internos;
+    }
+    public int completos(NodoArbol<T> nodoAct){
+        int completos = 0;
+        if (nodoAct.getDer() != null && nodoAct.getIzq() != null) {
+            completos++;
+        }
+        if (nodoAct.getIzq() != null) {
+            completos += nodoAct.getIzq().completos(nodoAct.getIzq());
+        }
+        if (nodoAct.getDer() != null) {
+            completos += nodoAct.getDer().completos(nodoAct.getDer());
+        }
+        return completos;
+
+
+    }
+
+}
